868c3777d21f527fac33205fe6a4540972869654
evo264714 <mh264714@gmail.com>
2023-02-10 15:15:58 +0600

    events

--- js/events.js
+++ js/events.js
@@ -1,8 +1,11 @@
 console.log('This is JS')
 
 //* Option 1: Directly write down on HTML 
+//*  <button onclick="console.log(65)">Anothe Button</button>
 
-//* Option 2: add onclick function
+//* Option 2: add onclick function on the html element
+//* <button onclick="makeRed()">Make Red</button>
+
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
@@ -21,3 +24,24 @@ const purpleButton = document.getElementById('make-purple');
     purpleButton.onclick = function makePurple(){
         document.body.style.backgroundColor = 'purple';
     }
+
+//* Option 4:
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink)
+
+function makePink(){
+    document.body.style.backgroundColor = 'pink';
+}
+
+//* Option 4 another:
+const makeGreenButton = document.getElementById('make-green');
+makeGreenButton.addEventListener('click', function makeGreen(){
+ document.body.style.backgroundColor = 'green';
+})
+
+//* Option 4 Final:
+document.getElementById('make-goldenrod').addEventListener('click', function(){
+    document.body.style.backgroundColor = "goldenrod";
+  })
+
+
